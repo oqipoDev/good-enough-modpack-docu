7e77a91dc8a1ff0c5f6b12d5008f38ffac85f6e5
oqipo Dev <oqipo.art@protonmail.com>
2023-04-03 20:34:25 -0600

    Functional views: Grid and Mod page

--- js/modrinthRequest.js
+++ js/modrinthRequest.js
@@ -1,24 +1,9 @@
+const MCVer = "1.19.2";
+const Loader = "fabric";
+
 function getMods(ids, func){
 	if (!Array.isArray(ids)) return;
 
-	const batchSize = 8;
-	let idList = [...ids];
-
-	let batches = [];
-
-	do {
-		let current = batches.length;
-		batches[current] = idList.slice(batchSize * current, (batchSize * (current + 1)) );
-		
-	} while(idList.length > batchSize);
-	
-	sendModrinthRequests([], batches, func);
-}
-
-function sendModrinthRequests(data, batches, func){
-	console.log(batches);
-}
-
-function getModrinthRequests(result, data, batches, func){
-
+	fetch(`https://api.modrinth.com/v2/projects?ids=${JSON.stringify(ids)}`)
+		.then((e) => e.json()).then((e) => window[func.name](e));
 }
